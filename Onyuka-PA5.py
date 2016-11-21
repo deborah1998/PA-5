5d0cad0e84622b308575eb455bdefbc1fed0674f
deborahonyuka <daonyuka@loyola.edu>
2016-11-21 09:51:57 -0500

    Algorithm for PA 5

---
+++ Onyuka-PA5.py
@@ -0,0 +1,20 @@
+
+
+
+import os
+# Purpose: read in a file from the user and return it once they give one that exists
+# Parameters: None
+# Return: file_name
+def file_reader():
+    #ask user for input
+    file_name = input("Please enter the file you would like to input: ")
+    #if input is invalid ask again
+    while not os.path.exists(file_name):
+        file_name = input("Invalid Input! Please enter the file you would like to input: ")
+    return file_name
+
+
+
+
+
+
