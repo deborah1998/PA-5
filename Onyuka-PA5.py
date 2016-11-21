65db7fb393ad1b91dfe94e1acb86adb895be5827
deborahonyuka <daonyuka@loyola.edu>
2016-11-21 17:25:20 -0500

    Algorithm for PA 5

--- Onyuka-PA5.py
+++ Onyuka-PA5.py
@@ -34,22 +34,44 @@ def read_file(filename):
     SystemExit(1)
     return earthquakes
 
+#Function Name:find_earthquakes_on_date
+#Purpose: find and output to a file all earthquakes that occured on a specific date
+#Parameters:the list earthquakes,outfilename
+#Return:none
+
 
 def find_earthquakes_on_date(outfile_name,earthquakes):
     outputfile=open(outfile_name)
-    earthquake_date=("Please enter a specific date in which the program will find all earthquakes for")
+    print("")
+    month=input("Please enter a specific month(two digits)")
+    day=input("Please enter a specific day(two digis)")
+    year=input("Please enter a specific year(four digits)")
+    earthquake_date=(month+"-"+day+"-"+year)
     for i in range (len(earthquakes)):
-        if earthquake_date == earthquake[0]:
-
-
-
+        if earthquake_date == earthquakes[0]:
+            print(earthquakes[0:9])
+        else:
+            print("This is an invalid date")
     outputfile.close()
 
+#Function Name:average_magnitude
+#Purpose:find the average of the earthquakes in the file
+#Paramaters: the list earthquakes
+#Return:average magnitude of earthquakes in file
+
+def average_magnitude (earthquakes):
 
 
 
 
 
-    outputfile.close()
+
+
+
+
+
+
+
+
 
 
