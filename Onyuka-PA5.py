156dd8e910c1795f3d791df8c34c04ee4c997690
deborahonyuka <daonyuka@loyola.edu>
2016-11-22 00:31:25 -0500

    Algorithm for PA 5

--- Onyuka-PA5.py
+++ Onyuka-PA5.py
@@ -49,8 +49,6 @@ def find_earthquakes_on_date(outfile_name,earthquakes):
     for i in range (len(earthquakes)):
         if earthquake_date == earthquakes[0][0]:
             print(earthquakes[0:9], file =outputfile)
-        else:
-            print("This is an invalid date!")
     outputfile.close()
     return earthquake_date
 
@@ -86,7 +84,8 @@ def num_of_earthquakes_in_distance(earthquakes,lat1,lon1,user_distance ):
 def menu():
     print("The choices you have to choose from to learn more about earthquakes is dates, magnitude, and distance")
     choice = input("What would you like to know about earthquakes?")
-    return
+    return choice
+
 
 
 def main():
@@ -94,24 +93,26 @@ def main():
     print("The purpose of this program is to analyze earthquake data based on your choice")
     new_file=file_reader()
     new_list =read_file(new_file)
-    choice=menu()
-    if choice.lower() == "dates":
-        outfile_name = input("Please enter the name of the output file:")
-        earth_dates =find_earthquakes_on_date(outfile_name,new_list)
-        print(earth_dates)
-    if choice.lower() =="magnitude":
-        avg_mag=average_magnitude(new_list)
-        print("The average magnitude of the earthquakes",avg_mag)
-    if choice.lower() == "distance":
-        lat1 = input("Please enter a specific latitude: ")
-        lon1 = input("Please enter a specific longitude:")
-        user_distance = ("Please enter a specific distance:")
-        num_distance = num_of_earthquakes_in_distance(new_list, lat1, lon1, user_distance)
-        print("The number of earthquakes within that distance is",num_distance)
-    choice2= input("Would you like to choose another option?")
+    choice2 = input("Would you like to choose another option?")
     while choice2.lower == "yes":
         choice2 = input("Would you like to choose another option?")
         menu()
+
+        choice=menu()
+        if choice.lower() == "dates":
+            outfile_name = input("Please enter the name of the output file:")
+            earth_dates =find_earthquakes_on_date(outfile_name,new_list)
+            print(earth_dates)
+        if choice.lower() =="magnitude":
+            avg_mag=average_magnitude(new_list)
+            print("The average magnitude of the earthquakes",avg_mag)
+        if choice.lower() == "distance":
+            lat1 = input("Please enter a specific latitude: ")
+            lon1 = input("Please enter a specific longitude:")
+            user_distance = ("Please enter a specific distance:")
+            num_distance = num_of_earthquakes_in_distance(new_list, lat1, lon1, user_distance)
+            print("The number of earthquakes within that distance is",num_distance)
+
 main()
 
 
