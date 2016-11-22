2bc2353d36b215482f02853cf42fde7eac62479b
deborahonyuka <daonyuka@loyola.edu>
2016-11-21 23:57:20 -0500

    Algorithm for PA 5

--- Onyuka-PA5.py
+++ Onyuka-PA5.py
@@ -54,6 +54,7 @@ def find_earthquakes_on_date(outfile_name,earthquakes):
         else:
             print("This is an invalid date!")
     outputfile.close()
+    return earthquake_date
 
 #Function Name:average_magnitude
 #Purpose:find the average of the earthquakes in the file
@@ -96,21 +97,21 @@ def main():
     print("The purpose of this program is to analyze earthquake data based on your choice")
     new_file=file_reader()
     new_list =read_file(new_file)
-
+    outfile_name=input("Please enter the name of the output file:")
     menu()
     if choice.lower() == "dates":
         earth_dates =find_earthquakes_on_date(outfile_name,new_list)
         print(earth_dates)
-    if choice2.lower =="magnitude":
+    if choice.lower =="magnitude":
         avg_mag=average_magnitude(new_list)
         print(avg_mag)
-    if choice2.lower == "distance":
+    if choice.lower == "distance":
         lat1 = input("Please enter a specific latitude: ")
         lon1 = input("Please enter a specific longitude:")
         user_distance = ("Please enter a specific distance:")
         num_distance = num_of_earthquakes_in_distance(new_list, lat1, lon1, user_distance)
         print(num_distance)
-    choice2 = input("Would you like to choose another option?")
+    choice2= input("Would you like to choose another option?")
     while choice2.lower == "yes":
         choice2 = input("Would you like to choose another option?")
         menu()
